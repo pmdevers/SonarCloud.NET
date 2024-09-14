c8eb20e313e8b9c5990054f86d5b1f103b4a974d
Patrick Evers <pmdevers@gmail.com>
2024-09-14 22:48:02 +0200

    Implement WebServicesApi (#49)

---
+++ test/SonarCloud.NET.Tests/Helper/SnapshotReader.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Net;
+using System.Runtime.CompilerServices;
+
+namespace SonarCloud.NET.Tests.Helper;
+public static class SnapshotReader<T>
+{
+    public static HttpResponseMessage Read(HttpStatusCode statusCode, string method)
+    {
+        var result = Read($"{statusCode}_{method}");
+        return new() {
+            StatusCode = statusCode,
+            Content = new StringContent(result)
+        };
+    }
+
+    public static string Read(string name)
+    {
+        var resourceName = $"SonarCloud.NET.Tests.Responses.{typeof(T).Name}.{name}.json";
+        using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+        if (stream == null)
+        {
+            return string.Empty;
+        }
+
+        using var reader = new StreamReader(stream);
+        var json = reader.ReadToEnd();
+        return json;
+    }
+}
---
+++ test/SonarCloud.NET.Tests/WebServicesApiTests.cs
@@ -0,0 +1,48 @@
+﻿using SonarCloud.NET.Tests.Helper;
+using System.Net;
+
+namespace SonarCloud.NET.Tests;
+public class WebServicesApiTests
+{
+    [Fact]
+    public async Task List_Should_Call()
+    {
+        var response = SnapshotReader<WebServicesApi>.Read(HttpStatusCode.OK, "list");
+
+        var handler = new RequestValidationHandler((r) =>
+        {
+            r.RequestUri.Should().Be("https://sonarcloud.io/api/webservices/list");
+            return true;
+        }, response);
+
+        var client = TestsHelper.GetClient(handler);
+
+
+        var result = await client.WebServices.List();
+
+        result.WebServices.Should().HaveCount(2);
+    }
+
+    [Fact]
+    public async Task Example()
+    {
+        var response = SnapshotReader<WebServicesApi>.Read(HttpStatusCode.OK, "example");
+
+        var handler = new RequestValidationHandler((r) =>
+        {
+            r.RequestUri.Should().Be("https://sonarcloud.io/api/webservices/response_example?action=action&controller=controller");
+            return true;
+        }, response);
+
+        var client = TestsHelper.GetClient(handler);
+
+
+        var result = await client.WebServices.ResponseExample(new()
+        {
+            Action = "action",
+            Controller = "controller"
+        });
+
+        result.Format.Should().NotBeNullOrEmpty();
+    }
+}
